6691173b114afae8d7c7f8769f4dd723f10881f4
Björn Marschollek <git@muffi.eu>
2018-06-07 18:28:30 +0100

    Add a solution for Hello World

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting contains important greeting functions
+package greeting
+
+// HelloWorld returns the string "Hello, World!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
