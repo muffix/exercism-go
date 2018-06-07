88bcd3e4f5307db9410e6a54111cd87e46e5ab23
Björn Marschollek <git@muffi.eu>
2018-06-07 18:23:08 +0100

    Add a solution for Gigasecond

---
+++ gigasecond/gigasecond.go
@@ -0,0 +1,9 @@
+// Package gigasecond provides time tools in the gigasecond sphere
+package gigasecond
+
+import "time"
+
+// Returns the time a gigasecond (10^9 seconds) after the receiver
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(time.Second * 1e9)
+}
