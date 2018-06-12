e5ca9a1787bcc9e1bdc7a7fe498e3a1d1bb0de64
Björn Marschollek <git@muffi.eu>
2018-06-12 16:16:35 +0100

    Add a solution for Reverse String

---
+++ reverse-string/reverse_string.go
@@ -0,0 +1,11 @@
+// Package reverse contains functions to reverse strings
+package reverse
+
+// String returns the reversed string
+func String(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
