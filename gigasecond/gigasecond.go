e7ea8cf5f64f8543e3e66d9b27a4d8f94b7e49c4
Björn Marschollek <git@muffi.eu>
2018-06-12 17:09:14 +0100

    Update the docstring for AddGigasecond

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -3,7 +3,7 @@ package gigasecond
 
 import "time"
 
-// Returns the time a gigasecond (10^9 seconds) after the receiver
+// AddGigasecond returns the time a gigasecond (10^9 seconds) after the receiver
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(time.Second * 1e9)
 }
