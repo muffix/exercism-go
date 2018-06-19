2d755852e944c6d335c0c70f95d5c4bdbf388b1e
Björn Marschollek <git@muffi.eu>
2018-06-19 19:39:08 +0100

    Add a solution for Leap

---
+++ leap/leap.go
@@ -0,0 +1,7 @@
+// Package leap contains leap year tools
+package leap
+
+// IsLeapYear returns whether a given year is a leap year
+func IsLeapYear(year int) bool {
+	return year%4 == 0 && year%100 != 0 || year%400 == 0
+}
